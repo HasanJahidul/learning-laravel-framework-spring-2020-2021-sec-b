e1b7a5e2ed9c18657a0435283d3330d0301ec7a9
HasanJahidul <joyaiub1@gmail.com>
2021-06-12 22:53:12 +0600

    Crud operation with laravel

---
+++ laratest/resources/views/user/details.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Details</title>
+</head>
+<body>
+	
+	<a href="/home"> Back</a> |
+	<a href="/logout"> Logout </a> 
+
+	<h2>User Details</h2>
+
+	<table border="1">
+		<tr>
+			<td>ID</td>
+			<td>{{$user['id']}}</td>
+		</tr>
+		<tr>
+			<td>Username</td>
+			<td>{{$user['name']}}</td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>{{$user['email']}}</td>
+		</tr>
+		<tr>
+			<td>Type</td>
+			<td>{{$user['type']}}</td>
+		</tr>
+	</table>
+	
+</body>
+</html>
